3b10f49df2af8da17dfafaba75061336e8e0e489
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-04-29 18:57:30 +0800

    initial pseudocode for the program: quiz reader

---
+++ assignment_10_quiz_reader.py
@@ -0,0 +1,7 @@
+#Assignment 10 Create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
+#read the text file from the original quiz creator
+#print the quiz questions randomly
+#user inputs answer
+#check if answer is correct
+#once completed all the questions, print how many answers user got correct and ask if user wants to restart the quiz
+#jump in joy
