9fd1a2b3fa242c38e733565c4d0888d0861dff90
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-04-29 19:13:07 +0800

    printed the questions text file so they show on the shell/panel

--- assignment_10_quiz_reader.py
+++ assignment_10_quiz_reader.py
@@ -1,6 +1,8 @@
 #Assignment 10 Create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
 #read the text file from the original quiz creator
 file = open("quiz_questions.txt", "r")
+f = file.readlines()
+print(f)
 #print the quiz questions randomly
 #user inputs answer
 #check if answer is correct
