2456a2af0faf158bc38ce584d0452f3a9611740a
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-04-29 19:43:50 +0800

    defining the variables for the question, options, and correct answer using lines and line strip

--- assignment_10_quiz_reader.py
+++ assignment_10_quiz_reader.py
@@ -3,12 +3,19 @@
 file = open("quiz_questions.txt", "r")
 f = file.readlines()
 #print the quiz questions randomly
+def ask_question(question_block):
+    # Initialize variables for the question, options, and correct answer
+    question = ""
+    options = []
+    correct_answer = ""
+    
 for i in range(5):
     print(f[i].strip())
 for i in range(5, 6):
     print(f[i].strip())
+    
 #user inputs answer
-answer = input("n/Enter the correct answer: ")
+answer = input("\nEnter the correct answer: ")
 
 #check if answer is correct
 #once completed all the questions, print how many answers user got correct and ask if user wants to restart the quiz
